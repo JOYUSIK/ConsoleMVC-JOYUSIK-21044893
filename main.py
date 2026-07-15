6ac2c5e11451cda1868d119dd010fd58cc2f8d3c
yousik.jo <aksksla9@naver.com>
2026-07-15 10:14:17 +0900

    feat: main.py 진입점 및 메뉴 루프 구현

---
+++ main.py
@@ -0,0 +1,32 @@
+from controller.item_controller import ItemController
+from view.item_view import ItemView
+
+MENU_ACTIONS = {
+    "1": "create",
+    "2": "list_all",
+    "3": "detail",
+    "4": "update",
+    "5": "delete",
+}
+
+
+def run():
+    controller = ItemController()
+    view = ItemView()
+
+    while True:
+        choice = view.show_menu()
+
+        if choice == "0":
+            print("\n 시스템을 종료합니다.")
+            break
+
+        action = MENU_ACTIONS.get(choice)
+        if action:
+            getattr(controller, action)()
+        else:
+            view.show_error("올바른 메뉴 번호를 입력해주세요.")
+
+
+if __name__ == "__main__":
+    run()
